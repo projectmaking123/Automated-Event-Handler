a20e3c296b93cfcecf690c884833c4d8662257cc
projectmaking123 <jason.89.wang@gmail.com>
2018-02-05 14:27:00 -0500

    mod schema

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20180205184939) do
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
-    t.float    "price"
+    t.decimal  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
