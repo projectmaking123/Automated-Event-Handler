4f2a68b7f19435655e32510d164390c49ba94a39
projectmaking123 <jason.89.wang@gmail.com>
2018-02-05 16:06:37 -0500

    added sass and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.create(name: "test", description: "hello", price: 12.33)
